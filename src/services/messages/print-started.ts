4752f427a953f98a0c8cbe2b5535e430f575c3b3
Rémi LACHAUX <contact@remi-lachaux.fr>
2024-10-12 18:00:39 +0200

    fix url project

--- src/services/messages/print-started.ts
+++ src/services/messages/print-started.ts
@@ -1,19 +1,21 @@
 import { EmbedBuilder } from "discord.js";
 
 import { NOTIFICATION_COLOR, NOTIFICATION_FOOTER_ICON, NOTIFICATION_FOOTER_TEXT } from "../../constants";
-import { getProjectURL, getScreenshotURL } from "../../libs/s3-storage";
+import { getScreenshotURL } from "../../libs/s3-storage";
 import type { Status } from "../../types/printer-status";
 
 export const printStarted = async (status: Status) => {
-  return new EmbedBuilder()
-    .setURL(await getProjectURL(status))
-    .setTitle("Démarrage de l'impression")
-    .setDescription(`L'imprimante ce prépare pour imprimer **${status.taskName}**`)
-    .setColor(NOTIFICATION_COLOR)
-    .setFooter({
-      text: NOTIFICATION_FOOTER_TEXT,
-      iconURL: NOTIFICATION_FOOTER_ICON
-    })
-    .setTimestamp(new Date())
-    .setImage(await getScreenshotURL());
+  return (
+    new EmbedBuilder()
+      // TODO fix SDCard file .setURL(await getProjectURL(status))
+      .setTitle("Démarrage de l'impression")
+      .setDescription(`L'imprimante ce prépare pour imprimer **${status.taskName}**`)
+      .setColor(NOTIFICATION_COLOR)
+      .setFooter({
+        text: NOTIFICATION_FOOTER_TEXT,
+        iconURL: NOTIFICATION_FOOTER_ICON
+      })
+      .setTimestamp(new Date())
+      .setImage(await getScreenshotURL())
+  );
 };
